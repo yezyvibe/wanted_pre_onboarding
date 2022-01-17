c9848810379341010bd2c20c56e8c3849910387f
yezyvibe <yeji0501@gmail.com>
2022-01-17 22:17:37 +0900

    feat: btn position and update css

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,26 +1,28 @@
+import styled from "styled-components";
 import React, { useRef } from "react";
 import styles from "../static/Main.module.css";
 
+const Box = styled.div`
+  height: 300px;
+  width: 1060px;
+`;
 export default function Main() {
   const container = useRef();
   let isCheck = false;
   const onClickPrev = () => {
-    container.current.style.transitionDuration = "1300ms";
-    container.current.style.transform = `translateX(50%)`;
+    container.current.style.transitionDuration = "900ms";
+    container.current.style.transform = `translateX(64%)`;
     container.current.ontransitionend = () => rearrange("prev");
     isCheck = true;
-    console.log("prev 눌렸을 때");
   };
   const onClickNext = () => {
-    container.current.style.transitionDuration = "1300ms";
-    container.current.style.transform = `translateX(-50%)`;
+    container.current.style.transitionDuration = "900ms";
+    container.current.style.transform = `translateX(-63%)`;
     container.current.ontransitionend = () => rearrange("next");
     isCheck = true;
-    console.log("next 눌렸을 때");
   };
 
   const rearrange = (btn) => {
-    console.log("rearrange 들어옴");
     container.current.removeAttribute("style");
     if (!isCheck) {
       return;
@@ -39,28 +41,26 @@ export default function Main() {
     <div>
       <div className={styles.window}>
         <div className={styles.container} ref={container}>
-          <div className={styles.cell}>
+          <Box className={styles.cell}>
             <img src="img/slide1.jpg" alt="slide1"></img>
-          </div>
-          <div className={styles.cell}>
+          </Box>
+          <Box className={styles.cell}>
             <img src="img/slide2.jpg" alt="slide2"></img>
-          </div>
-          <div className={styles.cell}>
+          </Box>
+          <Box className={styles.cell}>
             <img src="img/slide3.jpg" alt="slide3"></img>
-          </div>
-          <div className={styles.cell}>
+          </Box>
+          <Box className={styles.cell}>
             <img src="img/slide4.jpg" alt="slide4"></img>
-          </div>
-          <div className={styles.cell}>
+          </Box>
+          <Box className={styles.cell}>
             <img src="img/slide5.jpg" alt="slide5"></img>
-          </div>
+          </Box>
         </div>
-      </div>
-      <div className={styles.btnBox}>
-        <button className={styles.btn} onClick={onClickPrev}>
+        <button className={styles.btn_left} onClick={onClickPrev}>
           👈
         </button>
-        <button className={styles.btn} onClick={onClickNext}>
+        <button className={styles.btn_right} onClick={onClickNext}>
           👉
         </button>
       </div>
